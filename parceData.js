1c5147929cffcf805ba5f315e103e233a7473b8a
Shivum Arora <126220391+shakArora@users.noreply.github.com>
2023-07-12 22:33:51 -0400

    Update parceData.js

--- parceData.js
+++ parceData.js
@@ -1,48 +1,48 @@
 function parseData() {
-    fetch('data.txt')
-      .then(response => response.text())
-      .then(data => {
+  fetch('data.txt')
+    .then(response => response.text())
+    .then(data => {
+      if (!data) {
+        console.error('Empty data received from file.');
+        return;
+      }
 
-        const lines = data.split('\n');
-  
-        const days = [];
+      const lines = data.split('\n');
+      const days = [];
 
-        lines.forEach(line => {
+      lines.forEach(line => {
+        const parts = line.trim().split(',');
 
-          const parts = line.trim().split(',');
-  
-          const day = parts[0].trim();
-          const dayType = parts[1].trim();
-          const comment = parts[2].trim();
-  
-          const dayObj = {
-            date: day,
-            type: dayType,
-            comment: comment,
-          };
-  
-    
-          days.push(dayObj);
-        });
-  
-        const jsonData = JSON.stringify(days, null, 2);
+        const day = parts[0]?.trim();
+        const dayType = parts[1]?.trim();
+        const comment = parts[2]?.trim();
 
-        const dataUri =
-          'data:application/json;charset=utf-8,' +
-          encodeURIComponent(jsonData);
-        const link = document.createElement('a');
-        link.href = dataUri;
-        link.download = 'days.json';
-  
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+        const dayObj = {
+          date: day || '',
+          type: dayType || '',
+          comment: comment || '',
+        };
+
+        days.push(dayObj);
       });
-  }
 
-  setInterval(parseData, 60000);
-  parseData();
-  
+      const jsonData = JSON.stringify(days, null, 2);
+
+      const dataUri =
+        'data:application/json;charset=utf-8,' +
+        encodeURIComponent(jsonData);
+      const link = document.createElement('a');
+      link.href = dataUri;
+      link.download = 'days.json';
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+    .catch(error => {
+      console.error('Error fetching or processing data:', error);
+    });
+}
+
+setInterval(parseData, 60000);
+parseData();
