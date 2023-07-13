f81ba719877edfef1203001118810dde260608ea
Shivum Arora <126220391+shakArora@users.noreply.github.com>
2023-07-12 23:05:08 -0400

    Update parceData.js

--- parceData.js
+++ parceData.js
@@ -58,7 +58,7 @@ function parseData() {
             method: 'PUT',
             headers: {
               'Content-Type': 'application/json',
-              Authorization: `Bearer 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`, 
+              Authorization: `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`, 
             },
             body: JSON.stringify(requestBody),
           })
